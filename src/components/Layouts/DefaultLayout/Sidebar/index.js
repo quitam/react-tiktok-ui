9c84df84cf5059716fdfe2339a1c16bf1b488cad
quitam <tamphamasd@gmail.com>
2022-11-22 23:39:28 +0700

    e

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -5,11 +5,7 @@ import styles from './Sidebar.module.scss';
 const cx = classNames.bind(styles);
 
 const Sidebar = () => {
-    return (
-        <aside className={cx('wrapper')}>
-            <h2>Sidebar</h2>
-        </aside>
-    );
+    return <aside className={cx('wrapper')}></aside>;
 };
 
 export default Sidebar;
--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,14 +1,32 @@
+import React from 'react';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
-
-import React from 'react';
+import images from '~/assets/images';
 
 const cx = classNames.bind(styles);
 
 const Header = () => {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>dfsdfsad</div>
+            <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="Tiktok logo" />
+                </div>
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon icon={faSpinner} />
+
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+            </div>
         </header>
     );
 };
